c210d951250abc469716562b7a8199d3b5e7c269
Marcelo Morais <m2marcelo@gmail.com>
2020-05-03 20:40:58 +0200

    Adding kubeconfig variable

--- udacity-c3-deployment/k8s/deploy.sh
+++ udacity-c3-deployment/k8s/deploy.sh
@@ -2,14 +2,17 @@
 
  set -e
 
- configmap=`cat "./env-configmap.yaml" | sed "s/{{AWS_MEDIA_BUCKET}}/$AWS_MEDIA_BUCKET/g;s/{{AWS_PROFILE}}/$AWS_PROFILE/g;s/{{AWS_REGION}}/$AWS_REGION/g;s/{{POSTGRES_DATABASE}}/$POSTGRES_DATABASE/g;s/{{POSTGRES_HOST}}/$POSTGRES_HOST/g;s#{{APP_URL}}#$APP_URL#g"`
+ echo $PWD
+
+ dir=
+ configmap=`cat "udacity-c3-deployment/k8s/env-configmap.yaml" | sed "s/{{AWS_MEDIA_BUCKET}}/$AWS_MEDIA_BUCKET/g;s/{{AWS_PROFILE}}/$AWS_PROFILE/g;s/{{AWS_REGION}}/$AWS_REGION/g;s/{{POSTGRES_DATABASE}}/$POSTGRES_DATABASE/g;s/{{POSTGRES_HOST}}/$POSTGRES_HOST/g;s#{{APP_URL}}#$APP_URL#g"`
  echo "$configmap" | kubectl apply -f -
 
- secret=`cat "./env-secret.yaml" | sed "s/{{JWT_SECRET}}/$JWT_SECRET/g;s/{{POSTGRES_USERNAME}}/$POSTGRES_USERNAME/g;s/{{POSTGRES_PASSWORD}}/$POSTGRES_PASSWORD/g"`
+ secret=`cat "udacity-c3-deployment/k8s/env-secret.yaml" | sed "s/{{JWT_SECRET}}/$JWT_SECRET/g;s/{{POSTGRES_USERNAME}}/$POSTGRES_USERNAME/g;s/{{POSTGRES_PASSWORD}}/$POSTGRES_PASSWORD/g;s/{{KUBECONFIG_AWS}}/$KUBECONFIG_AWS/g"`
  echo "$secret" | kubectl apply -f -
 
  awsCredentials=`cat ~/.aws/credentials | base64`
- awsSecret=`cat "./aws-secret.yaml" | sed "s/{{AWS_CREDENTIALS}}/$awsCredentials/g"`
+ awsSecret=`cat "udacity-c3-deployment/k8s/aws-secret.yaml" | sed "s/{{AWS_CREDENTIALS}}/$awsCredentials/g"`
  echo "$awsSecret" | kubectl apply -f -
 
 kubectl apply -f ./backend-feed-deployment.yaml
