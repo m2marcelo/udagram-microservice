f4d5fef62e5db5587609b7db2f478aded2943af2
Marcelo Morais <m2marcelo@gmail.com>
2020-05-04 20:33:26 +0200

    updating config files

--- build/k8s/deploy.sh
+++ build/k8s/deploy.sh
@@ -27,6 +27,7 @@
  echo "Passo 1 DIR = $DIR"
 
  configmap=`cat "$DIR/env-configmap.yaml" | sed "s/{{AWS_MEDIA_BUCKET}}/$AWS_MEDIA_BUCKET/g;s/{{AWS_PROFILE}}/$AWS_PROFILE/g;s/{{AWS_REGION}}/$AWS_REGION/g;s/{{POSTGRES_DATABASE}}/$POSTGRES_DATABASE/g;s/{{POSTGRES_HOST}}/$POSTGRES_HOST/g;s#{{APP_URL}}#$APP_URL#g"`
+ 
  echo "$configmap" | kubectl apply -f -
 
  echo "Passo 2"
