18d446f4e137403cbb1c434c5d7ccbab364a828d
Marcelo Morais <m2marcelo@gmail.com>
2020-05-06 21:32:19 +0200

    Removing old scripts

--- build/k8s/deploy_services.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-kubectl apply -f ./backend-feed-service.yaml
-kubectl apply -f ./backend-user-service.yaml
-kubectl apply -f ./reverseproxy-service.yaml
-kubectl apply -f ./frontend-service.yaml
